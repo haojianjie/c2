32ca4bcde5358999f75799e86430ee2b29dcd917
Your Name <you@example.com>
2019-01-15 22:45:44 +0800

    取钱系统

--- main.c
+++ main.c
@@ -1,40 +1,58 @@
 #include <stdio.h>
 
-int main(){
-    int qian=0;
+int main()
+{
+    int qian = 0;
     printf("jinrucunquqianguanlixitong\n");
-    while(1){
+    while (1)
+    {
         int a;
         printf("1---cunqian\n");
         printf("2---quqian\n");
         printf("3---xianshijine\n");
         printf("4---tuichuchengxu\n");
         printf("qingshurugongnengbianhao\n");
-        scanf("%d",&a);
-if(a==1){
-    int cun;
-    printf("jinrucunqianxitong,qingshuruyaocunrudejine\n");
-    scanf("%d",&cun);
-    qian+=cun;
-    printf("cunruchenggong,dianjihuichejixu\n");
-    char x;
-    scanf("%c",&x);
-    scanf("%c",&x);
-
-}
-if(a==2){
-    
-}
-if(a==3){
-    
-}
-if(a==4){
-    printf("cunxujieshu");
-    break;
-}
-
-
+        scanf("%d", &a);
+        if (a == 1)
+        {
+            int cun;
+            printf("jinrucunqianxitong,qingshuruyaocunrudejine\n");
+            scanf("%d", &cun);
+            qian += cun;
+            printf("cunruchenggong,dianjihuichejixu\n");
+            char x;
+            scanf("%c", &x);
+            scanf("%c", &x);
+        }
+        if (a == 2)
+        {
+            int qu;
+            printf("qingshuruyaoqudejine\n");
+            scanf("%d", &qu);
+            if (qian < qu)
+            {
+                printf("yuebuzu,quqianshibai\n");
+                char x;
+                scanf("%c", &x);
+                scanf("%c", &x);
+            }
+            else
+            {
+                qian -= qu;
+                printf("quqianchenggong,dianjihuichetuichu\n");
+                char x;
+                scanf("%c", &x);
+                scanf("%c", &x);
+            }
+        }
+        if (a == 3)
+        {
+        }
+        if (a == 4)
+        {
+            printf("cunxujieshu");
+            break;
+        }
     }
     return 0;
-
 }
